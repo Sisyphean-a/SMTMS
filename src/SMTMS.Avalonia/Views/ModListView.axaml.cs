eb5bf5dd928710146d7c167fb3cdcccfded0bb79
xixifu <xia362108178@gmail.com>
2025-12-25 11:36:08 +0800

    perf：视图拆分

---
+++ src/SMTMS.Avalonia/Views/ModListView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SMTMS.Avalonia.Views;
+
+public partial class ModListView : UserControl
+{
+    public ModListView()
+    {
+        InitializeComponent();
+    }
+}
